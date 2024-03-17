22af9447b8f5a45aa27f0894bc6e5a2fed0f7d01
Viacheslav Shylko <116965660+Laslold@users.noreply.github.com>
2024-03-17 19:17:14 +0200

    bag fix svg sprite

--- src/components/Ui/FavoriteButton.vue
+++ src/components/Ui/FavoriteButton.vue
@@ -1,10 +1,27 @@
 <template lang="">
   <button class="btn">
-    <svg class="icon" v-if="!store">
-      <use xlink:href="../../assets/sprite.svg#star-empty"></use>
+    <svg
+      class="icon"
+      v-if="!store"
+      width="12px"
+      height="12px"
+      viewBox="0 0 32 32"
+    >
+      <path
+        d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798zM16 23.547l-6.983 3.671 1.334-7.776-5.65-5.507 7.808-1.134 3.492-7.075 3.492 7.075 7.807 1.134-5.65 5.507 1.334 7.776-6.983-3.671z"
+      ></path>
     </svg>
-    <svg class="icon" v-else :style="{ fill: '#ff0000' }">
-      <use xlink:href="../../assets/sprite.svg#star-full"></use>
+    <svg
+      class="icon"
+      v-else
+      :style="{ fill: '#ff0000' }"
+      width="12px"
+      height="12px"
+      viewBox="0 0 32 32"
+    >
+      <path
+        d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"
+      ></path>
     </svg>
     <slot></slot>
   </button>
